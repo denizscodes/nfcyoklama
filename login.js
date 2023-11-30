0fb677cfc4b9a37f4cddb15abba04f75c5f8668b
Deniz.codes <117769520+denizscodes@users.noreply.github.com>
2023-11-30 20:16:40 +0300

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function logFormData() {
     var ogrenciAdi = document.getElementById("ogrenci_adi").value;
     var sifre = document.getElementById("sifre").value;
 
-    window.location.href = "http://127.0.0.1:5500/" + ogrenciAdi + ".html";
+    window.location.href = "https://denizscodes.github.io/nfcyoklama/" + ogrenciAdi + ".html";
     console.log("Öğrenci Adı: " + ogrenciAdi);
     console.log("Şifre: " + sifre);
 
